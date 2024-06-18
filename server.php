8e573c5c573240510de9cedd6a775c93669a9e55
Amir <78240549+Askay35@users.noreply.github.com>
2024-06-18 12:23:59 +0300

    Update server.php

--- server.php
+++ server.php
@@ -5,13 +5,4 @@ echo "<table>";
 foreach (apache_request_headers() as $k => $v){
   echo "<tr><td style=\"border: 2px solid black;\">$k</td><td style=\"border: 2px solid black;\">$v</td></tr>";
 }
-setcookie("suka", "pidor");
-setcookie("s124a", "p124r");
-setcookie("su1125ka", "p125i");
-setcookie("s2156uka", "p125125idor");
-setcookie("s2156uka", "p125125idor");
-setcookie("y134", "315124r");
-setcookie("yandex", "123");
-setcookie("124y", "gsdfse3");
-
 echo "</table>";
